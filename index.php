8a2f19cfb74f440628da917a4fd7f5bb841a9214
hiphop200199 <hiphop200199@gmail.com>
2024-10-30 01:26:46 +0800

    make dialog

--- index.php
+++ index.php
@@ -23,41 +23,44 @@
      inkscape:groupmode="layer"
      id="layer1">
     <path
-       style="fill:none;stroke:#6d0346;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
+       style="fill:none;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
        d="m 181.78789,671.06314 c 11.50531,-109.80331 25.24705,-252.07689 37.02767,-385.11799 7.80338,-88.12527 374.85653,-90.99526 369.0661,-0.96706 -8.95501,139.23032 -14.49091,325.0791 -43.74824,358.063 -26.67491,30.07255 -270.40019,34.13917 -270.40019,34.13917"
        id="path1"
        sodipodi:nodetypes="csssc" />
     <path
-       style="fill:none;stroke:#6d0346;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
+       style="fill:none;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
        d="m 275.30878,366.75043 c 43.34043,-49.71116 142.25241,54.24662 234.64928,-6.78499"
        id="path2"
        sodipodi:nodetypes="cc" />
     <path
-       style="fill:none;stroke:#6d0346;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
+       style="fill:none;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
        d="m 267.54208,435.78608 c 43.34043,-49.71116 142.25241,54.24662 234.64928,-6.78499"
        id="path3"
        sodipodi:nodetypes="cc" />
     <path
-       style="fill:none;stroke:#6d0346;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
+       style="fill:none;stroke-width:15.0009;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
        d="m 267.54208,504.82173 c 43.34043,-49.71116 142.25241,54.24662 234.64928,-6.78499"
        id="path4"
        sodipodi:nodetypes="cc" />
     <path
-       style="fill:none;stroke:#6d0346;stroke-width:5.24943;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:0.999999"
+       style="fill:none;stroke-width:5.24943;stroke-linecap:round;stroke-linejoin:round;stroke-opacity:0.999999"
        d="m 452.68885,615.20081 c 4.34331,7.07615 8.59665,-1.60613 9.11295,-0.72982 3.78273,6.42038 6.79527,-1.24654 7.78053,0.3751 6.43259,10.58742 24.73071,-15.07067 26.6833,-30.76925 1.54681,-12.43622 -6.79423,36.81977 10.80147,29.2452"
        id="path5"
        sodipodi:nodetypes="csssc" />
   </g>
 </svg>       
-<dialog>
+</div>
+<dialog >
+  
         <form >
+        <button id="close-modal">✖</button>
             <input type="text" placeholder="email...">
             <input type="password" name="" id="" placeholder="password...">
+            <section>
             <button type="submit">send</button>
             <a href="">register</a>
+            </section>
             <a href="">forgot password?</a>
         </form>
-       </dialog>
-
-</div>
+</dialog>
 <?php require_once './parts/foot.php'; ?>
--- script.js
+++ script.js
@@ -5,9 +5,17 @@ $(function() { //ready function
   
    
   
-  
+ $('#login').on('click', function () {
+    document.querySelector('dialog').showModal();
+    document.querySelector('dialog').classList.add('open');
+ }); 
     
-  
+$('#close-modal').on('click', function (e) {
+   e.preventDefault()
+   document.querySelector('dialog').classList.remove('open');
+   setTimeout(()=>document.querySelector('dialog').close(),700)
+   
+});  
    
 
    
